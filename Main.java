3e63d40d78e2db064f29ab513cdd4bcdd7316c0c
cardze <43008882+cardze@users.noreply.github.com>
2021-01-02 19:11:14 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+package game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        stage.setTitle("MENU");
+        Scene scene = new Scene(root,900,600);
+
+        stage.setScene(scene);
+        stage.show();
+//        stage.setScene(new Scene(root, 900, 600));
+//        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Home_work_game_UI_design_practice
+ 
